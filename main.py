5da99c123bd7cf8d8fcf175c98b4faf4e49a09ab
nemesisrc <rajdeepchakraborty.asansol@gmail.com>
2025-05-18 23:30:10 +0530

    commit03-18.05.25

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import asyncio
 
+# coroutine function
+# main() returns a coroutine object
 async def main():
     print("starting point of the main coroutine")
 
-# run the main coroutine
+
+
+# start the eventloop and run the main coroutine
 asyncio.run(main())
+
