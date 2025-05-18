a785f343366eb6f8fcd8f9e5fe2724a93375490c
nemesisrc <rajdeepchakraborty.asansol@gmail.com>
2025-05-18 23:05:02 +0530

    commit01-18.05.25

---
+++ main.py
@@ -0,0 +1,7 @@
+import asyncio
+
+async def main():
+    print("starting point of the main coroutine")
+
+# run the main coroutine
+asyncio.run(main())
---
+++ readme.txt
@@ -0,0 +1,5 @@
+1. eventloop
+2. coroutine
+3. tasks
+4. futures
+5. synchronization
